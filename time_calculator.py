b016ede26420fd3f9158222a3779cca087f6edb9
Anthony Perozeni <anthony.perozeni777@gmail.com>
2022-08-07 17:59:23 +0200

    1ere modif time_calculator.py

---
+++ time_calculator.py
@@ -0,0 +1,11 @@
+def add_time(time, time_to_add):
+
+    if "AM" not in time or "PM" not in time:
+        print("Erreur! Veuillez renseigner 'AM' ou 'PM'")
+
+    else :
+        for i in time :
+            
+
+
+
